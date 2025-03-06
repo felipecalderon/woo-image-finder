a5e5faa1ff95eb6bf44ee769a931a0b830ac6e58
Felipe <felipe.calderon321@gmail.com>
2025-03-06 20:53:33 -0300

    hidden register temporally

--- src/app/api/auth/register/route.ts
+++
@@ -1,8 +0,0 @@
-import { saveUser } from '@/actions/user.action'
-import { NextRequest, NextResponse } from 'next/server'
-
-export const POST = async (req: NextRequest) => {
-    const user = await req.json()
-    await saveUser(user)
-    return NextResponse.json({ success: true })
-}
