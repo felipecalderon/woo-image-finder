74b04d997aa0f0af16b0b0e07921870af6876e0d
Felipe <felipe.calderon321@gmail.com>
2025-03-08 22:38:38 -0300

    added filters text

--- src/lib/clean-text.ts
+++ src/lib/clean-text.ts
@@ -1,6 +1,6 @@
 export const cleanText = (texto: string) => {
     return texto
-        .replace(/\d+\s*(mm|cm|cms|mt|mts)\b/gi, '') // Elimina unidades (ej: 25mm -> 25)
+        .replace(/\d+\s*(mm|cm|cms|mt|mts|lts|grs|lbs|pzas|pcs)\b/gi, '') // Elimina unidades (ej: 25mm -> 25)
         .replace(/\s*x\s*/gi, ' x ') // Elimina espacios en "x" (ej: 25 x 3 -> 25x3)
         .replace(/['"]/g, '') // Elimina comillas simples y dobles
         .trim() // Elimina espacios extra al inicio o final
